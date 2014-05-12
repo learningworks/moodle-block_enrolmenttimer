f1b549a4ad65c52ba7390b4af9b8cb61a914be81
SMERKY <aaron@aaronleggett.com>
2014-05-12 22:13:50 +1200

    Update version.php

--- enrolmenttimer/version.php
+++ enrolmenttimer/version.php
@@ -25,3 +25,6 @@
 $plugin->version = 2014051215;  // YYYYMMDDHH (year, month, day, 24-hr time)
 $plugin->requires = 2010112400; // YYYYMMDDHH (This is the release version for Moodle 2.0)
 $plugin->cron = 86400; 			//cron runs daily
+$plugin->component = 'block_enrolmenttimer';
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = 'Moodle 2.6.2+ (Build: 20140314)';
