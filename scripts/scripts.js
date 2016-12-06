f79f007a1d9b3d5e0e066c62931b462ecb47cd5d
Mathew May <mathewm@hotmail.co.nz>
2016-12-07 11:16:47 +1300

    Fix several Travis issues

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -27,6 +27,92 @@ require(['jquery'], function($) { // Moodle needs this to recognise $ https://do
     var forceTwoDigits = false;
 
     $(document).ready(function() {
+
+        function getDisplayedOptions(){
+            var children = $('.block_enrolmenttimer .active .timer-wrapper').find('.timerNum');
+
+            for (var i = children.length - 1; i >= 0; i--) {
+                var arrayKey = $(children[i]).attr('data-id');
+                arrayKeys.push(arrayKey);
+            }
+        }
+
+        function populateWithData(){
+            for (var i = arrayKeys.length - 1; i >= 0; i--) {
+                var option = $('.block_enrolmenttimer .active .text-desc .' + arrayKeys[i]).text();
+                options[arrayKeys[i]] = option;
+            }
+        }
+
+        function makeTimestamp(){
+            for (var i = arrayKeys.length - 1; i >= 0; i--) {
+                switch(arrayKeys[i]){
+                    case 'seconds':
+                        timestamp += parseInt(options[arrayKeys[i]], 10);
+                        break;
+
+                    case 'minutes':
+                        timestamp += parseInt(options[arrayKeys[i]], 10) * 60;
+                        break;
+
+                    case 'hours':
+                        timestamp += parseInt(options[arrayKeys[i]], 10) * 3600;
+                        break;
+
+                    case 'days':
+                        timestamp += parseInt(options[arrayKeys[i]], 10) * 86400;
+                        break;
+
+                    case 'weeks':
+                        timestamp += parseInt(options[arrayKeys[i]], 10) * 604800;
+                        break;
+
+                    case 'months':
+                        timestamp += parseInt(options[arrayKeys[i]], 10) * 2592000;
+                        break;
+
+                    case 'years':
+                        timestamp += parseInt(options[arrayKeys[i]], 10) * 31536000;
+                        break;
+                }
+            }
+        }
+
+        function updateLiveCounter() {
+            timestamp--;
+            var time = timestamp;
+            var tokens = ['years', 'months', 'weeks', 'days', 'hours', 'minutes', 'seconds'];
+            var units = ['31536000', '2592000', '604800', '86400', '3600', '60', '1'];
+
+            for (var i = 0; i < tokens.length; i++) {
+
+                if (arrayKeys.indexOf(tokens[i]) != -1) {
+                    if (time >= units[i]) {
+                        var count = Math.floor(time / units[i]);
+                        updateMainCounter(tokens[i], count);
+                        time = time - (count * units[i]);
+                    } else {
+                        updateMainCounter(tokens[i], 0);
+                    }
+                }
+            }
+        }
+
+        function updateMainCounter(counter, time){
+            var html = '';
+            if(forceTwoDigits === true && time.toString().length == 1){
+                html += '<span class="timerNumChar" data-id="0">0</span>';
+                html += '<span class="timerNumChar" data-id="1">' + time.toString() + '</span>';
+            } else {
+                for (var i = 0; i < time.toString().length; i++) {
+                    html += '<span class="timerNumChar" data-id="' + i + '">' + time.toString().charAt(i) + '</span>';
+                }
+            }
+
+            $('.block_enrolmenttimer .active .timer-wrapper .timerNum[data-id="' + counter + '"]').html(html);
+            $('.block_enrolmenttimer .active .text-desc .' + counter).html(time);
+        }
+
         if($('.block_enrolmenttimer .active').length > 0){
             getDisplayedOptions();
             populateWithData();
@@ -42,92 +128,4 @@ require(['jquery'], function($) { // Moodle needs this to recognise $ https://do
             forceTwoDigits = true;
         }
     });
-
-    function getDisplayedOptions(){
-        var children = $('.block_enrolmenttimer .active .timer-wrapper').find('.timerNum');
-
-        for (var i = children.length - 1; i >= 0; i--) {
-            var arrayKey = $(children[i]).attr('data-id');
-            arrayKeys.push(arrayKey);
-        };
-    }
-
-    function populateWithData(){
-        var counts = [];
-
-        for (var i = arrayKeys.length - 1; i >= 0; i--) {
-            var option = $('.block_enrolmenttimer .active .text-desc .' + arrayKeys[i]).text();
-            options[arrayKeys[i]] = option;
-        };
-    }
-
-    function makeTimestamp(){
-        for (var i = arrayKeys.length - 1; i >= 0; i--) {
-            switch(arrayKeys[i]){
-                case 'seconds':
-                    timestamp += parseInt(options[arrayKeys[i]], 10);
-                    break;
-
-                case 'minutes':
-                    timestamp += parseInt(options[arrayKeys[i]], 10) * 60;
-                    break;
-
-                case 'hours':
-                    timestamp += parseInt(options[arrayKeys[i]], 10) * 3600;
-                    break;
-
-                case 'days':
-                    timestamp += parseInt(options[arrayKeys[i]], 10) * 86400;
-                    break;
-
-                case 'weeks':
-                    timestamp += parseInt(options[arrayKeys[i]], 10) * 604800;
-                    break;
-
-                case 'months':
-                    timestamp += parseInt(options[arrayKeys[i]], 10) * 2592000;
-                    break;
-
-                case 'years':
-                    timestamp += parseInt(options[arrayKeys[i]], 10) * 31536000;
-                    break;
-            }
-        };
-    }
-
-    function updateLiveCounter() {
-        timestamp--;
-        var time = timestamp;
-        var tokens = ['years', 'months', 'weeks', 'days', 'hours', 'minutes', 'seconds'];
-        var units = ['31536000', '2592000', '604800', '86400', '3600', '60', '1'];
-
-        for (var i = 0; i < tokens.length; i++) {
-
-            if (arrayKeys.indexOf(tokens[i]) != -1) {
-                if (time >= units[i]) {
-                    var count = Math.floor(time / units[i]);
-                    updateMainCounter(tokens[i], count);
-                    time = time - (count * units[i]);
-                } else {
-                    updateMainCounter(tokens[i], 0);
-                }
-            }
-        }
-    }
-
-    function updateMainCounter(counter, time){
-        var html = '';
-        if(forceTwoDigits == true && time.toString().length == 1){
-            html += '<span class="timerNumChar" data-id="0">0</span>';
-            html += '<span class="timerNumChar" data-id="1">' + time.toString() + '</span>';
-        } else {
-            for (var i = 0; i < time.toString().length; i++) {
-                html += '<span class="timerNumChar" data-id="' + i + '">' + time.toString().charAt(i) + '</span>';
-            };
-        }
-
-        $('.block_enrolmenttimer .active .timer-wrapper .timerNum[data-id="' + counter + '"]').html(html);
-        $('.block_enrolmenttimer .active .text-desc .' + counter).html(time);
-    }
-
 });
